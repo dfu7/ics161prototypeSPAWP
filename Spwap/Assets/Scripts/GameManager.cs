2978cffc9da22bb010d071902e333aaf0b1c435f
LocT02 <xxthegreeksxx@gmail.com>
2022-05-07 18:47:46 -0700

    Scene set up + mixer/audiomanager

--- Spwap/Assets/Scripts/GameManager.cs
+++ Spwap/Assets/Scripts/GameManager.cs
@@ -5,9 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public void SceneLoad()
+    #region Instance
+    private static GameManager instance;
+    public static GameManager Instance
     {
-        SceneManager.LoadScene("Level1", LoadSceneMode.Additive);
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+                if (instance == null)
+                {
+                    instance = new GameObject("Spawned AudioManager", typeof(GameManager)).GetComponent<GameManager>();
+                }
+            }
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+    #endregion
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void LevelLoad()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void MenuLoad()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
     public void Quit()
     {
