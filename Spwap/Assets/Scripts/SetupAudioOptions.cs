14f0db436115f77f1806ef5a43472f40c77a37d4
LocT02 <xxthegreeksxx@gmail.com>
2022-05-29 05:45:28 -0700

    stuff

---
+++ Spwap/Assets/Scripts/SetupAudioOptions.cs
@@ -0,0 +1,25 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetupAudioOptions : MonoBehaviour
+{
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    void Start()
+    {
+        masterSlider.onValueChanged.AddListener(AudioManager.Instance.SetMasterVolume);
+        musicSlider.onValueChanged.AddListener(AudioManager.Instance.SetMusicVolume);
+        SFXSlider.onValueChanged.AddListener(AudioManager.Instance.SetSFXVolume);
+        
+        AudioManager.Instance.sliderMaster = masterSlider;
+        AudioManager.Instance.sliderMusic = musicSlider;
+        AudioManager.Instance.sliderSFX = SFXSlider;
+
+        masterSlider.value = PlayerPrefs.GetFloat("MasterSliderPref");
+        musicSlider.value = PlayerPrefs.GetFloat("MusicSliderPref");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXSliderPref");
+    }
+}
